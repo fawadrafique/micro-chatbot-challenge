292bd4f0ebe32501270135fe07463c9639595237
Muhammad Fawad Rafique <fawadrafique@hotmail.com>
2020-07-28 14:26:08 +0200

    Displays input message

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
+const inputValue = document.getElementById("input");
+const message = document.getElementById("message");
+const enter = document.getElementById("enter");
 // document.addEventListener("DOMContentLoaded", () => {}) can also be used
 document.onreadystatechange = function () {
-  console.log("Ready");
-  const inputValue = document.getElementById("input");
-  const message = document.getElementById("message");
-  const enter = document.getElementById("enter");
-
   if (document.readyState === "interactive") {
-    console.log("interactive");
-
-    enter.onclick = () => callback(message, inputValue);
+    enter.onclick = () => callback();
     document.getElementById("input").addEventListener("keydown", (event) => {
       if (event.code === "Enter") {
         callback();
@@ -16,8 +12,10 @@ document.onreadystatechange = function () {
     });
   }
 };
-const callback = function (message, inputValue) {
+const callback = function () {
+  console.log("callback");
   let input = inputValue.value;
   inputValue.value = "";
-  message.innertext = input;
+  message.innerHTML = `<span class="has-text-link has-text-weight-bold">${input}: </span>
+  <span class="has-text-black-bis">${input}</span>`;
 };
